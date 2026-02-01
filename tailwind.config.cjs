8a08a9786805d2c454b7286a190e0bc0c61ede0a
kinza-ubaidullah <kinzaubaidullah62@gmail.com>
2026-02-02 00:27:12 +0500

    commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
     content: [
         "./index.html",
-        "./App.tsx",
-        "./index.tsx",
-        "./components/**/*.{js,ts,jsx,tsx}",
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./services/**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
         extend: {
